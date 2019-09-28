28b605ba3d7b2df348848c7ec598b4ee61225c18
Steve Rumizen <steverumizen@gmail.com>
2019-09-28 13:03:17 -0600

    Add route for checklist screen

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { Header } from '../../Components/Header/Header';
 import { LoginScreen } from '../../Components/LoginScreen/LoginScreen';
+import { ChecklistScreen } from '../ChecklistScreen/ChecklistScreen';
 import './App.css';
 
 export const App = () => {
   return (
     <main>
       <Header></Header>
-      <Route exact path='/' component={LoginScreen}></Route>
+      <Route exact path="/" component={LoginScreen}></Route>
+      <Route exact path="/checklist" component={ChecklistScreen}></Route>g
     </main>
   );
 };
