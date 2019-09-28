11f3468dffe41feaa7aacef0988988196cd9aef4
Steve Rumizen <steverumizen@gmail.com>
2019-09-28 15:46:23 -0600

    Add plan action to update reducer

--- src/Reducers/preIncidentReducer.js
+++ src/Reducers/preIncidentReducer.js
@@ -1,5 +1,13 @@
 export const preIncidentReducer = (state = [], action) => {
   switch (action.type) {
+    case "LOG_ANSWER":
+      return state.map(question => {
+        if (question.id === action.id) {
+          return { ...question, answer };
+        } else {
+          return question;
+        }
+      });
     default:
       return state;
   }
