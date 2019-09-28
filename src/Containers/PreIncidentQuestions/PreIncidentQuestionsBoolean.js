e853b1b559ce9f059e5f08492bcc0b96ef7f5d31
baldm0mma <jev.forsberg@gmail.com>
2019-09-28 15:43:08 -0600

    Make pre incident questions boolean a class

--- src/Containers/PreIncidentQuestions/PreIncidentQuestionsBoolean.js
+++ src/Containers/PreIncidentQuestions/PreIncidentQuestionsBoolean.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logAnswer } from '../../Actions/index';
 
-export const PreIncidentQuestionsBoolean = ({ question }) => {
-  return (
-    <article>
-      <h3>{`Question ${question.id}`}</h3>
-      <p>{question.question}</p>
-      <input
-        type='radio'
-        value='yes'
-        name='checked'
-        // onChange={}
-      />
-      Yes
-      <input
-        type='radio'
-        value='no'
-        name='checked'
-        // onChange={}
-      />
-      No
-    </article>
-  );
-};
+export class PreIncidentQuestionsBoolean extends Component {
+  state = {
+    answer: null
+  };
+
+  handleSubmit = () => {
+    const { logAnswer, id, advanceQuestion } = this.props;
+    logAnswer(id, this.state.answer);
+    advanceQuestion();
+  };
+
+  render = () => {
+    return (
+      <article>
+        <p>{this.props.question}</p>
+        <button onClick={() => this.setState({ answer: true })}>Yes</button>
+        <button onClick={() => this.setState({ answer: false })}>No</button>
+        <button onClick={this.handleSubmit}>{this.props.buttonText}</button>
+      </article>
+    );
+  };
+}
+
+export const mapDispatchToProps = dispatch => ({
+  logAnswer: (id, answer) => dispatch(logAnswer(id, answer))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(PreIncidentQuestionsBoolean);
