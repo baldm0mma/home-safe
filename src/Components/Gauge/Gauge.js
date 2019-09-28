7bf9dfa39e7f847e958ca2bf60263c5a65635843
Antonio Fry <antoniofry2000@gmail.com>
2019-09-28 12:12:15 -0600

    implement working CircularProgressbar component

--- src/Components/Gauge/Gauge.js
+++ src/Components/Gauge/Gauge.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-const Gauge = () => {
+export const Gauge = () => {
+  const percentage = 95;
   return (
     <div>
-      
+      <CircularProgressbar
+        value={percentage}
+        text={`${percentage}%`}
+        styles={buildStyles({
+          pathColor: '#7FFF00',
+          textColor: '#f88',
+          trailColor: '#d6d6d6',
+        })}
+      />
     </div>
   )
 }
