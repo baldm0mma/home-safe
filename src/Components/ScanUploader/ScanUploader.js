2e1a49465cfe682d81b62170b47749f3f31ec79a
Antonio Fry <antoniofry2000@gmail.com>
2019-09-28 16:39:23 -0600

    Initialize ScanUploader component file

---
+++ src/Components/ScanUploader/ScanUploader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ScanUploader extends Component {
+  state = {
+    Upload: ''
+  }
+
+  render() {
+    return (
+      <form>
+        
+      </form>
+    )
+  }
+}
