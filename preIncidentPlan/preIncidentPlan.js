09214bff2c619766380ac294c8b9fc98f5b798d9
baldm0mma <jev.forsberg@gmail.com>
2019-09-28 14:23:49 -0600

    Add pre-incident plan questionare

---
+++ preIncidentPlan/preIncidentPlan.js
@@ -0,0 +1,62 @@
+const preIncidentPlan = [
+  {
+    id: 1,
+    question: 'Do you have a gate or locked entrance to your residence?',
+    answer: ''
+  },
+  {
+    id: 2,
+    question: 'Do you have a fire hydrant with one block, or 200ft of your residence?',
+    answer: ''
+  },
+  {
+    id: 3,
+    question: 'Do you have a pond, creek, or river within 1/4 mile of your residence?',
+    answer: ''
+  },
+  {
+    id: 4,
+    question: 'What type of roof do you have?',
+    answer: ''
+  },
+  {
+    id: 5,
+    question: 'Do you have a basement?',
+    answer: ''
+  },
+  {
+    id: 6,
+    question: 'How many direct exits (doors) do you have in your residence?',
+    answer: ''
+  },
+  {
+    id: 7,
+    question: 'Do you have access to your attic from the interior of you residence?',
+    answer: ''
+  },
+  {
+    id: 8,
+    question: 'Do you have a fire alarm system?',
+    answer: ''
+  },
+  {
+    id: 9,
+    question: 'Do you have a fire suppression system?',
+    answer: ''
+  },
+  {
+    id: 10,
+    question: 'How many smoke detectors are in your residence?',
+    answer: ''
+  },
+  {
+    id: 11,
+    question: 'Do you have a carbon monoxide detector in your residence?',
+    answer: ''
+  },
+  {
+    id: 12,
+    question: 'Are you storing any hazardous materials? If so, where?',
+    answer: ''
+  }
+]
