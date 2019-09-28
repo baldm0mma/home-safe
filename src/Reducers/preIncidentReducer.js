58396541e7ef8ac5ed814a239aa5f29bf3ca946f
baldm0mma <jev.forsberg@gmail.com>
2019-09-28 16:29:57 -0600

    Add options for radio and checkbox questions in pre incident questions reducer

--- src/Reducers/preIncidentReducer.js
+++ src/Reducers/preIncidentReducer.js
@@ -1,84 +1,89 @@
 export const defaultQuestions = [
   {
     id: 1,
-    question: "Do you have a gate or locked entrance to your residence?",
-    type: "boolean",
+    question: 'Do you have a gate or locked entrance to your residence?',
+    type: 'boolean',
     answer: false
   },
   {
     id: 2,
     question:
-      "Do you have a fire hydrant within one block, or 200ft of your residence?",
-    type: "boolean",
+      'Do you have a fire hydrant within one block, or 200ft of your residence?',
+    type: 'boolean',
     answer: false
   },
   {
     id: 3,
     question:
-      "Do you have a pond, creek, or river within 1/4 mile of your residence?",
-    type: "boolean",
+      'Do you have a pond, creek, or river within 1/4 mile of your residence?',
+    type: 'boolean',
     answer: false
   },
   {
     id: 4,
-    question: "What type of roof do you have?",
-    type: "radio",
-    answer: ""
+    question: 'What type of roof do you have?',
+    type: 'radio',
+    options: ['Flat', 'Arhced', 'Pitched', 'Hip', 'Dome', 'Other'],
+    answer: ''
   },
   {
     id: 5,
-    question: "Do you have a basement?",
-    type: "radio",
-    answer: ""
+    question: 'Do you have a basement?',
+    type: 'radio',
+    options: ['No', 'Daylight Basement', 'Walkout Basement'],
+    answer: ''
   },
   {
     id: 6,
-    question: "How many direct exits (doors) do you have in your residence?",
-    type: "number",
+    question: 'How many direct exits (doors) do you have in your residence?',
+    type: 'number',
     answer: 0
   },
   {
     id: 7,
     question:
-      "Do you have access to your attic from the interior of your residence?",
-    type: "radio",
-    answer: ""
+      'Do you have access to your attic from the interior of your residence?',
+    type: 'radio',
+    options: ["Yes", "No", "I don't have an attic"],
+    answer: ''
   },
   {
     id: 8,
-    question: "Do you have a fire alarm system?",
-    type: "boolean",
+    question: 'Do you have a fire alarm system?',
+    type: 'boolean',
     answer: false
   },
   {
     id: 9,
-    question: "Do you have a fire suppression system?",
-    type: "checkbox",
+    question: 'Do you have a fire suppression system?',
+    type: 'checkbox',
+    options: ["Yes, wet", "Yes, dry chemical", "Yes, foam", "Yes, Halon", "No"],
     answer: []
   },
   {
     id: 10,
-    question: "How many smoke detectors are in your residence?",
-    type: "number",
+    question: 'How many smoke detectors are in your residence?',
+    type: 'number',
     answer: 0
   },
   {
     id: 11,
-    question: "Do you have a carbon monoxide detector in your residence?",
-    type: "boolean",
+    question: 'Do you have a carbon monoxide detector in your residence?',
+    type: 'boolean',
     answer: false
   },
   {
     id: 12,
-    question: "Are you storing any hazardous materials? If so, where?",
-    type: "checkbox",
+    question: 'Are you storing any hazardous materials? If so, where?',
+    type: 'checkbox',
+    options: ["Garage", "Basement", "Kitchen", "Bathroom", "Attic", "No"],
     answer: []
   }
 ];
 
 export const preIncidentReducer = (state = defaultQuestions, action) => {
   switch (action.type) {
-    case "LOG_ANSWER":
+    case 'LOG_ANSWER':
       return state.map(question => {
         if (question.id === action.id) {
           return { ...question, answer: action.answer };
