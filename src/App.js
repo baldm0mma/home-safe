e75fb9e52cbad80b81c6e90e10aa812546c0836d
Steve Rumizen <steverumizen@gmail.com>
2019-09-28 12:02:23 -0600

    Prepare checklist components for merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import { ChecklistScreen } from "./ChecklistScreen";
 
 function App() {
   return (
     <div className="App">
       hi
-      <ChecklistScreen />
     </div>
   );
 }
