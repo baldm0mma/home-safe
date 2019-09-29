7fc81c3cde901f44b0557f4a85d88d1d31324a44
Vinton Teo <taulago@ymail.com>
2019-09-28 18:58:42 -0600

    Add Summary component to /summary Route.

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -3,7 +3,8 @@ import { Route } from 'react-router-dom';
 import { Header } from '../../Components/Header/Header';
 import { LoginScreen } from '../../Components/LoginScreen/LoginScreen';
 import ChecklistScreen from '../ChecklistScreen/ChecklistScreen';
-import PreIncidentScreen from "../PreIncidentScreen/PreIncidentScreen";
+import PreIncidentScreen from '../PreIncidentScreen/PreIncidentScreen';
+import { Summary } from '../../Components/Summary/Summary';
 import './App.css';
 
 export const App = () => {
@@ -14,6 +15,7 @@ export const App = () => {
         <Route exact path="/" component={LoginScreen}></Route>
         <Route exact path="/checklist" component={ChecklistScreen}></Route>
         <Route exact path="/plan" component={PreIncidentScreen}></Route>
+        <Route exact path="/summary" component={Summary}></Route>
       </section>
     </main>
   );
