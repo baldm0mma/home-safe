647955807648648de5468c9ed4cb30daafa8d9de
baldm0mma <jev.forsberg@gmail.com>
2019-09-29 09:05:43 -0600

    Add box styling to responder plan and bottom margin to login modal

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import Gauge from "../Gauge/Gauge";
-import "./Header.scss";
+import Gauge from '../Gauge/Gauge';
+import './Header.scss';
 
 export const Header = () => {
   return (
     <header>
-      <h1>Home Safe</h1>
-      <div className="header-gauge-wrapper">
-        <p className="progress-text">Safety Progress</p>
+      <section className='title-container'>
+        <h1>Home Safe</h1>
+        <p className='tagline'>Your safety is our #1 concern</p>
+      </section>
+      <div className='header-gauge-wrapper'>
+        <p className='progress-text'>Safety Progress</p>
         <Gauge />
       </div>
     </header>
   );
-}
+};
