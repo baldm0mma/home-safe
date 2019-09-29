aa0f8e9c65821036e9ff5aa799762eba6ac8bcb4
Vinton Teo <taulago@ymail.com>
2019-09-29 03:06:05 -0600

    Create case to handle updateProgress action.

--- src/Reducers/progressReducer.js
+++ src/Reducers/progressReducer.js
@@ -1,9 +1,11 @@
 export const progressReducer = (state = 0, action) => {
   switch (action.type) {
     case 'ADD_PROGRESS':
-      return state += action.percentage;
+      return (state += action.percentage);
     case 'SUBTRACT_PROGRESS':
-      return state -= action.percentage;
+      return (state -= action.percentage);
+    case 'UPDATE_PROGRESS':
+      return action.percentage;
     default:
       return state;
   }
