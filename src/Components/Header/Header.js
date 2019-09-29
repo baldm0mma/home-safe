084c3a5624c27f1ca9b9b0f113b98e5e6543d18f
baldm0mma <jev.forsberg@gmail.com>
2019-09-29 09:35:35 -0600

    Add home link styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Gauge from '../Gauge/Gauge';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 export const Header = () => {
   return (
     <header>
-      <section className='title-container'>
-        <h1>Home Safe</h1>
-        <p className='tagline'>Your safety is our #1 concern</p>
-      </section>
+      <NavLink to='/' className='home-link'>
+        <section className='title-container'>
+          <h1>Home Safe</h1>
+          <p className='tagline'>Your safety is our #1 concern</p>
+        </section>
+      </NavLink>
       <div className='header-gauge-wrapper'>
         <p className='progress-text'>Safety Progress</p>
         <Gauge />
