8d34a51812634cf8dea2b2da1425f21480b8e699
Antonio Fry <antoniofry2000@gmail.com>
2019-09-28 19:06:29 -0600

    Style ScanUploader and display selected file

--- src/Components/ScanUploader/ScanUploader.js
+++ src/Components/ScanUploader/ScanUploader.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import './ScanUploader.scss'
 
 class ScanUploader extends Component {
   state = {
-    houseScan: ''
+    houseScan: '',
+    submitted: false
   }
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ submitted: true });
+  }
+
   render() {
     return (
-      <form>
-        <label htmlFor="house-scan">Upload 3-D Scan of House</label>
+      <form className="scan-form" onSubmit={(e) => this.handleSubmit(e)}>
+        <label className="house-scan-label" htmlFor="house-scan">Upload 3-D Scan of House</label>
         <input 
           id="house-scan"
-          className="upload-button"
+          className="scan-upload-button"
           name="houseScan"
           type="file"
-          accept=".xyz"
-        />  
+          accept=".jpg"
+          value={this.state.houseScan}
+          onChange={(e) => this.handleChange(e)}
+        />
+        {this.state.houseScan !== '' && 
+          <div className="selected-file-container">
+            <p className="selected-file">{this.state.houseScan}</p>
+          </div>
+        }
+        {this.state.houseScan === '' && 
+          <div className="selected-file-container">
+            <p className="selected-file">No File Selected</p>
+          </div>
+        }
+        <button className="scan-submit-button">Submit Scan</button>
       </form>
     )
   }
